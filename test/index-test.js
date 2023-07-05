6e02d6525c49411c9154dd9f83e7e620642843c1
= <peterndirangu0005@gmail.com>
2023-07-05 21:12:11 +0300

    Done with assignment

--- test/index-test.js
+++ test/index-test.js
@@ -3,7 +3,6 @@ const path = require( 'path' );
 var chai = require( 'chai' );
 chai.use( require( 'chai-fs' ) );
 
-// i want to test this
 describe( 'This assignment', () => {
   it( 'has been correctly cloned to your local environment', () => {
     chai.assert.isDirectory( './.git', 'no ".git" folder was found within "welcome-completing-assignment". Use "git init" to create one' );
